1329db32e1f92c75a42fa401070c1a365bd38d62
Cristian Garcia <cristian@MacBook-Pro-de-Cristian.local>
2023-03-21 12:37:22 -0300

    feat: agregado swagger al proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   //esto es para usar express como plataforma
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Proyecto Antartida')
+    .setDescription('Documentacion de la API del proyecto antartico')
+    .setVersion('1.0')
+    .addTag('antartida')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
